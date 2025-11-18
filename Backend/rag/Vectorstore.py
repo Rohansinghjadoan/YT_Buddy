f9675961512e11b4fe637ef803230da6cbdd6ce9
rohan <rohansinghjadoan@gmail.com>
2025-11-19 02:12:23 +0530

    complete basic stucture

--- Backend/rag/Vectorstore.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.vectorstores import FAISS
-
-index_cache = {}
-
-def save_index(video_id, index):
-    index_cache[video_id] = index
-
-def get_index(video_id):
-    return index_cache.get(video_id)
